b05a4ff5a247140c280d63a02863fd61ce448e65
Jonas Etzbach <jonas.etzbach@cjt.de>
2019-12-20 10:22:32 +0100

    Revert "Revert "--all""
    This reverts commit 8c3d5612476f923b2bbf41f26716b8744d317815.

--- src/main/java/net/md_5/itag/iTag.java
+++ src/main/java/net/md_5/itag/iTag.java
@@ -1,20 +1,12 @@
 package net.md_5.itag;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.EnumWrappers.PlayerInfoAction;
-import com.comphenix.protocol.wrappers.PlayerInfoData;
-import com.comphenix.protocol.wrappers.WrappedGameProfile;
-import com.google.common.base.Preconditions;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-import lombok.Getter;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -25,156 +17,148 @@ import org.kitteh.tag.AsyncPlayerReceiveNameTagEvent;
 import org.kitteh.tag.PlayerReceiveNameTagEvent;
 import org.kitteh.tag.TagAPI;
 
-public class iTag extends JavaPlugin implements Listener
-{
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.EnumWrappers.PlayerInfoAction;
+import com.comphenix.protocol.wrappers.PlayerInfoData;
+import com.comphenix.protocol.wrappers.WrappedGameProfile;
+import com.google.common.base.Preconditions;
 
-    @Getter
-    private static iTag instance;
-    private TagAPI tagAPI;
-    private Map<Integer, Player> entityIdMap;
-    private static final int[] uuidSplit = new int[]
-    {
-        0, 8, 12, 16, 20, 32
-    };
+import lombok.Getter;
 
-    @Override
-    public void onEnable()
-    {
-        instance = this;
-        entityIdMap = new HashMap<Integer, Player>();
-        tagAPI = new TagAPI( this );
+public class iTag extends JavaPlugin implements Listener {
 
-        for ( Player player : getServer().getOnlinePlayers() )
-        {
-            entityIdMap.put( player.getEntityId(), player );
-        }
+	@Getter
+	private static iTag instance;
+	private TagAPI tagAPI;
+	private Map<Integer, Player> entityIdMap;
+	private static final int[] uuidSplit = new int[] { 0, 8, 12, 16, 20, 32 };
 
-        getServer().getPluginManager().registerEvents( this, this );
-        ProtocolLibrary.getProtocolManager().addPacketListener( new PacketAdapter( this, PacketType.Play.Server.PLAYER_INFO )
-        {
-            @Override
-            public void onPacketSending(PacketEvent event)
-            {
-                if ( event.getPacket().getPlayerInfoAction().read( 0 ) != PlayerInfoAction.ADD_PLAYER )
-                {
-                    return;
-                }
+	@Override
+	public void onEnable() {
+		instance = this;
+		entityIdMap = new HashMap<Integer, Player>();
+		tagAPI = new TagAPI(this);
 
-                List<PlayerInfoData> newPlayerInfo = new ArrayList<PlayerInfoData>();
-                for ( PlayerInfoData playerInfo : event.getPacket().getPlayerInfoDataLists().read( 0 ) )
-                {
-                    Player player;
-                    if ( playerInfo == null || playerInfo.getProfile() == null || ( player = getServer().getPlayer( playerInfo.getProfile().getUUID() ) ) == null )
-                    {
-                        // Unknown Player
-                        newPlayerInfo.add( playerInfo );
-                        continue;
-                    }
+		for (Player player : getServer().getOnlinePlayers()) {
+			entityIdMap.put(player.getEntityId(), player);
+		}
 
-                    newPlayerInfo.add( new PlayerInfoData(
-                            getSentName( player.getEntityId(), playerInfo.getProfile(), event.getPlayer() ),
-                            playerInfo.getPing(), playerInfo.getGameMode(), playerInfo.getDisplayName()
-                    ) );
-                }
+		getServer().getPluginManager().registerEvents(this, this);
+		ProtocolLibrary.getProtocolManager()
+				.addPacketListener(new PacketAdapter(this, PacketType.Play.Server.PLAYER_INFO) {
+					@Override
+					public void onPacketSending(PacketEvent event) {
+						if (event.getPacket().getPlayerInfoAction().read(0) != PlayerInfoAction.ADD_PLAYER) {
+							return;
+						}
 
-                event.getPacket().getPlayerInfoDataLists().write( 0, newPlayerInfo );
-            }
-        } );
-    }
+						List<PlayerInfoData> newPlayerInfo = new ArrayList<PlayerInfoData>();
+						for (PlayerInfoData playerInfo : event.getPacket().getPlayerInfoDataLists().read(0)) {
+							Player player;
+							if (playerInfo == null || playerInfo.getProfile() == null
+									|| (player = getServer().getPlayer(playerInfo.getProfile().getUUID())) == null) {
+								// Unknown Player
+								newPlayerInfo.add(playerInfo);
+								continue;
+							}
 
-    @EventHandler
-    public void onJoin(PlayerJoinEvent event)
-    {
-        entityIdMap.put( event.getPlayer().getEntityId(), event.getPlayer() );
-    }
+							newPlayerInfo.add(new PlayerInfoData(
+									getSentName(player.getEntityId(), playerInfo.getProfile(), event.getPlayer()),
+									playerInfo.getPing(), playerInfo.getGameMode(), playerInfo.getDisplayName()));
+						}
 
-    @EventHandler
-    public void onQuit(PlayerQuitEvent event)
-    {
-        entityIdMap.remove( event.getPlayer().getEntityId() );
-    }
+						event.getPacket().getPlayerInfoDataLists().write(0, newPlayerInfo);
+					}
+				});
+	}
 
-    @Override
-    public void onDisable()
-    {
-        ProtocolLibrary.getProtocolManager().removePacketListeners( this );
+	public static iTag getInstance() {
+		return instance;
+	}
 
-        entityIdMap.clear();
-        entityIdMap = null;
-        tagAPI = null;
-        instance = null;
-    }
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		entityIdMap.put(event.getPlayer().getEntityId(), event.getPlayer());
+	}
 
-    private WrappedGameProfile getSentName(int sentEntityId, WrappedGameProfile sent, Player destinationPlayer)
-    {
-        Preconditions.checkState( getServer().isPrimaryThread(), "Can only process events on main thread." );
+	@EventHandler
+	public void onQuit(PlayerQuitEvent event) {
+		entityIdMap.remove(event.getPlayer().getEntityId());
+	}
 
-        Player namedPlayer = entityIdMap.get( sentEntityId );
-        if ( namedPlayer == null )
-        {
-            // They probably were dead when we reloaded
-            return sent;
-        }
+	@Override
+	public void onDisable() {
+		ProtocolLibrary.getProtocolManager().removePacketListeners(this);
 
-        PlayerReceiveNameTagEvent oldEvent = new PlayerReceiveNameTagEvent( destinationPlayer, namedPlayer, sent.getName() );
-        getServer().getPluginManager().callEvent( oldEvent );
+		entityIdMap.clear();
+		entityIdMap = null;
+		tagAPI = null;
+		instance = null;
+	}
 
-        StringBuilder builtUUID = new StringBuilder();
-        if ( !sent.getId().contains( "-" ) )
-        {
-            for ( int i = 0; i < uuidSplit.length - 1; i++ )
-            {
-                builtUUID.append( sent.getId().substring( uuidSplit[i], uuidSplit[i + 1] ) ).append( "-" );
-            }
-        } else
-        {
-            builtUUID.append( sent.getId() );
-        }
-        AsyncPlayerReceiveNameTagEvent newEvent = new AsyncPlayerReceiveNameTagEvent( destinationPlayer, namedPlayer, oldEvent.getTag(), UUID.fromString( builtUUID.toString() ) );
-        getServer().getPluginManager().callEvent( newEvent );
+	private WrappedGameProfile getSentName(int sentEntityId, WrappedGameProfile sent, Player destinationPlayer) {
+		Preconditions.checkState(getServer().isPrimaryThread(), "Can only process events on main thread.");
 
-        return new WrappedGameProfile( newEvent.getUUID(), newEvent.getTag().substring( 0, Math.min( newEvent.getTag().length(), 16 ) ) );
-    }
+		Player namedPlayer = entityIdMap.get(sentEntityId);
+		if (namedPlayer == null) {
+			// They probably were dead when we reloaded
+			return sent;
+		}
 
-    public void refreshPlayer(Player player)
-    {
-        Preconditions.checkState( isEnabled(), "Not Enabled!" );
-        Preconditions.checkNotNull( player, "player" );
+		PlayerReceiveNameTagEvent oldEvent = new PlayerReceiveNameTagEvent(destinationPlayer, namedPlayer,
+				sent.getName());
+		getServer().getPluginManager().callEvent(oldEvent);
 
-        for ( Player playerFor : player.getWorld().getPlayers() )
-        {
-            refreshPlayer( player, playerFor );
-        }
-    }
+		StringBuilder builtUUID = new StringBuilder();
+		if (!sent.getId().contains("-")) {
+			for (int i = 0; i < uuidSplit.length - 1; i++) {
+				builtUUID.append(sent.getId().substring(uuidSplit[i], uuidSplit[i + 1])).append("-");
+			}
+		} else {
+			builtUUID.append(sent.getId());
+		}
+		AsyncPlayerReceiveNameTagEvent newEvent = new AsyncPlayerReceiveNameTagEvent(destinationPlayer, namedPlayer,
+				oldEvent.getTag(), UUID.fromString(builtUUID.toString()));
+		getServer().getPluginManager().callEvent(newEvent);
 
-    public void refreshPlayer(final Player player, final Player forWhom)
-    {
-        Preconditions.checkState( isEnabled(), "Not Enabled!" );
-        Preconditions.checkNotNull( player, "player" );
-        Preconditions.checkNotNull( forWhom, "forWhom" );
+		return new WrappedGameProfile(newEvent.getUUID(),
+				newEvent.getTag().substring(0, Math.min(newEvent.getTag().length(), 16)));
+	}
 
-        if ( player != forWhom && player.getWorld() == forWhom.getWorld() && forWhom.canSee( player ) )
-        {
-            forWhom.hidePlayer( player );
-            getServer().getScheduler().scheduleSyncDelayedTask( this, new Runnable()
-            {
-                public void run()
-                {
-                    forWhom.showPlayer( player );
-                }
-            }, 2 );
-        }
-    }
+	public void refreshPlayer(Player player) {
+		Preconditions.checkState(isEnabled(), "Not Enabled!");
+		Preconditions.checkNotNull(player, "player");
 
-    public void refreshPlayer(Player player, Set<Player> forWhom)
-    {
-        Preconditions.checkState( isEnabled(), "Not Enabled!" );
-        Preconditions.checkNotNull( player, "player" );
-        Preconditions.checkNotNull( forWhom, "forWhom" );
+		for (Player playerFor : player.getWorld().getPlayers()) {
+			refreshPlayer(player, playerFor);
+		}
+	}
 
-        for ( Player playerFor : forWhom )
-        {
-            refreshPlayer( player, playerFor );
-        }
-    }
+	public void refreshPlayer(final Player player, final Player forWhom) {
+		Preconditions.checkState(isEnabled(), "Not Enabled!");
+		Preconditions.checkNotNull(player, "player");
+		Preconditions.checkNotNull(forWhom, "forWhom");
+
+		if (player != forWhom && player.getWorld() == forWhom.getWorld() && forWhom.canSee(player)) {
+			forWhom.hidePlayer(player);
+			getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+				public void run() {
+					forWhom.showPlayer(player);
+				}
+			}, 2);
+		}
+	}
+
+	public void refreshPlayer(Player player, Set<Player> forWhom) {
+		Preconditions.checkState(isEnabled(), "Not Enabled!");
+		Preconditions.checkNotNull(player, "player");
+		Preconditions.checkNotNull(forWhom, "forWhom");
+
+		for (Player playerFor : forWhom) {
+			refreshPlayer(player, playerFor);
+		}
+	}
 }
--- src/main/java/org/kitteh/tag/TagAPI.java
+++ src/main/java/org/kitteh/tag/TagAPI.java
@@ -1,22 +1,26 @@
 package org.kitteh.tag;
 
-import com.google.common.base.Throwables;
 import java.io.File;
+import java.io.InputStream;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Logger;
 import java.util.regex.Pattern;
-import lombok.Delegate;
-import lombok.Getter;
-import net.md_5.itag.iTag;
+
+import org.bukkit.Server;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.Listener;
+import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.InvalidDescriptionException;
 import org.bukkit.plugin.InvalidPluginException;
 import org.bukkit.plugin.Plugin;
@@ -27,121 +31,212 @@ import org.bukkit.plugin.RegisteredListener;
 import org.bukkit.plugin.SimplePluginManager;
 import org.bukkit.plugin.UnknownDependencyException;
 
-public class TagAPI extends PluginBase implements PluginLoader
-{
+import com.avaje.ebean.EbeanServer;
+import com.google.common.base.Throwables;
 
-    public Plugin loadPlugin(File file) throws InvalidPluginException, UnknownDependencyException
-    {
-        throw new UnsupportedOperationException( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
-    }
+import lombok.Delegate;
+import lombok.Getter;
+import net.md_5.itag.iTag;
 
-    public PluginDescriptionFile getPluginDescription(File file) throws InvalidDescriptionException
-    {
-        throw new UnsupportedOperationException( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
-    }
+public class TagAPI extends PluginBase implements PluginLoader {
 
-    public Pattern[] getPluginFileFilters()
-    {
-        throw new UnsupportedOperationException( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
-    }
+	public Plugin loadPlugin(File file) throws InvalidPluginException, UnknownDependencyException {
+		throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+																		// Tools | Templates.
+	}
 
-    public Map<Class<? extends Event>, Set<RegisteredListener>> createRegisteredListeners(Listener listener, Plugin plugin)
-    {
-        throw new UnsupportedOperationException( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
-    }
+	public PluginDescriptionFile getPluginDescription(File file) throws InvalidDescriptionException {
+		throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+																		// Tools | Templates.
+	}
 
-    public void enablePlugin(Plugin plugin)
-    {
-        throw new UnsupportedOperationException( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
-    }
+	public Pattern[] getPluginFileFilters() {
+		throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+																		// Tools | Templates.
+	}
 
-    private interface Excludes
-    {
+	public Map<Class<? extends Event>, Set<RegisteredListener>> createRegisteredListeners(Listener listener,
+			Plugin plugin) {
+		throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+																		// Tools | Templates.
+	}
 
-        PluginLoader getPluginLoader();
+	public void enablePlugin(Plugin plugin) {
+		throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+																		// Tools | Templates.
+	}
 
-        PluginDescriptionFile getDescription();
+	private interface Excludes {
 
-        String getName();
-    }
-    @Delegate(excludes = Excludes.class, types =
-    {
-        CommandExecutor.class, TabCompleter.class, Plugin.class
-    })
-    private final iTag parent;
-    @Getter
-    private PluginDescriptionFile description;
-    private List<Plugin> plugins;
-    private Map<String, Plugin> lookupNames;
+		PluginLoader getPluginLoader();
 
-    public TagAPI(iTag parent)
-    {
-        this.parent = parent;
+		PluginDescriptionFile getDescription();
 
-        plugins = (List<Plugin>) getObj( SimplePluginManager.class, parent.getServer().getPluginManager(), "plugins" );
-        lookupNames = (Map<String, Plugin>) getObj( SimplePluginManager.class, parent.getServer().getPluginManager(), "lookupNames" );
+		String getName();
+	}
 
-        StringWriter write = new StringWriter();
-        parent.getDescription().save( write );
-        String yaml = write.toString().replaceAll( "iTag", "TagAPI" );
+	@Delegate(excludes = Excludes.class, types = { CommandExecutor.class, TabCompleter.class, Plugin.class })
+	private final iTag parent;
+	@Getter
+	private PluginDescriptionFile description;
+	private List<Plugin> plugins;
+	private Map<String, Plugin> lookupNames;
 
-        try
-        {
-            description = new PluginDescriptionFile( new StringReader( yaml ) );
-        } catch ( InvalidDescriptionException ex )
-        {
-            throw Throwables.propagate( ex );
-        }
+	public TagAPI(iTag parent) {
+		this.parent = parent;
 
-        plugins.add( this );
-        lookupNames.put( getName(), this );
-    }
+		plugins = (List<Plugin>) getObj(SimplePluginManager.class, parent.getServer().getPluginManager(), "plugins");
+		lookupNames = (Map<String, Plugin>) getObj(SimplePluginManager.class, parent.getServer().getPluginManager(),
+				"lookupNames");
 
-    public PluginLoader getPluginLoader()
-    {
-        return this;
-    }
+		StringWriter write = new StringWriter();
+		parent.getDescription().save(write);
+		String yaml = write.toString().replaceAll("iTag", "TagAPI");
 
-    public void disablePlugin(Plugin plugin)
-    {
-        plugins.remove( plugin );
-        lookupNames.remove( plugin.getName() );
-    }
+		try {
+			description = new PluginDescriptionFile(new StringReader(yaml));
+		} catch (InvalidDescriptionException ex) {
+			throw Throwables.propagate(ex);
+		}
 
-    private static Object getObj(Class<?> clazz, Object owner, String name)
-    {
-        try
-        {
-            Field field = clazz.getDeclaredField( name );
-            field.setAccessible( true );
-            return field.get( owner );
-        } catch ( Exception ex )
-        {
-            throw Throwables.propagate( ex );
-        }
-    }
+		plugins.add(this);
+		lookupNames.put(getName(), this);
+	}
 
-    public static void refreshPlayer(Player player)
-    {
-        if ( iTag.getInstance() != null )
-        {
-            iTag.getInstance().refreshPlayer( player );
-        }
-    }
+	public PluginLoader getPluginLoader() {
+		return this;
+	}
 
-    public static void refreshPlayer(Player player, Player forWhom)
-    {
-        if ( iTag.getInstance() != null )
-        {
-            iTag.getInstance().refreshPlayer( player, forWhom );
-        }
-    }
+	public void disablePlugin(Plugin plugin) {
+		plugins.remove(plugin);
+		lookupNames.remove(plugin.getName());
+	}
 
-    public static void refreshPlayer(Player player, Set<Player> forWhom)
-    {
-        if ( iTag.getInstance() != null )
-        {
-            iTag.getInstance().refreshPlayer( player, forWhom );
-        }
-    }
+	private static Object getObj(Class<?> clazz, Object owner, String name) {
+		try {
+			Field field = clazz.getDeclaredField(name);
+			field.setAccessible(true);
+			return field.get(owner);
+		} catch (Exception ex) {
+			throw Throwables.propagate(ex);
+		}
+	}
+
+	public static void refreshPlayer(Player player) {
+		if (iTag.getInstance() != null) {
+			iTag.getInstance().refreshPlayer(player);
+		}
+	}
+
+	public static void refreshPlayer(Player player, Player forWhom) {
+		if (iTag.getInstance() != null) {
+			iTag.getInstance().refreshPlayer(player, forWhom);
+		}
+	}
+
+	public static void refreshPlayer(Player player, Set<Player> forWhom) {
+		if (iTag.getInstance() != null) {
+			iTag.getInstance().refreshPlayer(player, forWhom);
+		}
+	}
+
+	public File getDataFolder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PluginDescriptionFile getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public FileConfiguration getConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public InputStream getResource(String filename) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void saveConfig() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void saveDefaultConfig() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void saveResource(String resourcePath, boolean replace) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void reloadConfig() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Server getServer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void onDisable() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onEnable() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean isNaggable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setNaggable(boolean canNag) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public EbeanServer getDatabase() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Logger getLogger() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
