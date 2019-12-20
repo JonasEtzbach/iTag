ad80f4e9face95c754807aa72064d7cfa4241230
Jonas Etzbach <jonas.etzbach@cjt.de>
2019-12-20 10:19:18 +0100

    --all

--- src/main/java/org/kitteh/tag/AsyncPlayerReceiveNameTagEvent.java
+++ src/main/java/org/kitteh/tag/AsyncPlayerReceiveNameTagEvent.java
@@ -1,70 +1,75 @@
 package org.kitteh.tag;
 
-import com.google.common.base.Preconditions;
 import java.util.UUID;
-import lombok.Getter;
+
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public class AsyncPlayerReceiveNameTagEvent extends Event
-{
+import com.google.common.base.Preconditions;
 
-    private static final HandlerList handlers = new HandlerList();
-    /*========================================================================*/
-    @Getter
-    private final Player player;
-    @Getter
-    private final Player namedPlayer;
-    @Getter
-    private String tag;
-    @Getter
-    private UUID UUID;
-    @Getter
-    private boolean tagModified;
-    @Getter
-    private boolean UUIDModified;
+import lombok.Getter;
 
-    public AsyncPlayerReceiveNameTagEvent(Player who, Player namedPlayer, String initialName, UUID uuid)
-    {
-        Preconditions.checkNotNull( who, "who" );
-        Preconditions.checkNotNull( namedPlayer, "namedPlayer" );
-        Preconditions.checkNotNull( initialName, "initialName" );
-        Preconditions.checkNotNull( uuid, "uuid" );
+public class AsyncPlayerReceiveNameTagEvent extends Event {
 
-        this.player = who;
-        this.namedPlayer = namedPlayer;
-        this.tag = initialName;
-        this.tagModified = namedPlayer.getName().equals( initialName );
-        this.UUID = uuid;
-    }
+	private static final HandlerList handlers = new HandlerList();
+	/* ======================================================================== */
+	@Getter
+	private final Player player;
+	@Getter
+	private final Player namedPlayer;
+	@Getter
+	private String tag;
+	@Getter
+	private UUID UUID;
+	@Getter
+	private boolean tagModified;
+	@Getter
+	private boolean UUIDModified;
 
-    public boolean setTag(String tag)
-    {
-        Preconditions.checkNotNull( tag, "tag" );
+	public AsyncPlayerReceiveNameTagEvent(Player who, Player namedPlayer, String initialName, UUID uuid) {
+		Preconditions.checkNotNull(who, "who");
+		Preconditions.checkNotNull(namedPlayer, "namedPlayer");
+		Preconditions.checkNotNull(initialName, "initialName");
+		Preconditions.checkNotNull(uuid, "uuid");
 
-        this.tag = tag;
-        this.tagModified = true;
+		this.player = who;
+		this.namedPlayer = namedPlayer;
+		this.tag = initialName;
+		this.tagModified = namedPlayer.getName().equals(initialName);
+		this.UUID = uuid;
+	}
 
-        return tag.length() < 16;
-    }
+	public String getTag() {
+		return tag;
+	}
 
-    public void setUUID(UUID uuid)
-    {
-        Preconditions.checkNotNull( uuid, "uuid" );
+	public UUID getUUID() {
+		return UUID;
+	}
 
-        this.UUID = uuid;
-        this.UUIDModified = true;
-    }
+	public boolean setTag(String tag) {
+		Preconditions.checkNotNull(tag, "tag");
 
-    @Override
-    public HandlerList getHandlers()
-    {
-        return handlers;
-    }
+		this.tag = tag;
+		this.tagModified = true;
 
-    public static HandlerList getHandlerList()
-    {
-        return handlers;
-    }
+		return tag.length() < 16;
+	}
+
+	public void setUUID(UUID uuid) {
+		Preconditions.checkNotNull(uuid, "uuid");
+
+		this.UUID = uuid;
+		this.UUIDModified = true;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
 }
--- src/main/java/org/kitteh/tag/PlayerReceiveNameTagEvent.java
+++ src/main/java/org/kitteh/tag/PlayerReceiveNameTagEvent.java
@@ -1,52 +1,53 @@
 package org.kitteh.tag;
 
-import com.google.common.base.Preconditions;
-import lombok.Getter;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
 
-public class PlayerReceiveNameTagEvent extends PlayerEvent
-{
+import com.google.common.base.Preconditions;
 
-    private static final HandlerList handlers = new HandlerList();
-    /*========================================================================*/
-    @Getter
-    private final Player namedPlayer;
-    @Getter
-    private String tag;
-    @Getter
-    private boolean modified;
+import lombok.Getter;
 
-    public PlayerReceiveNameTagEvent(Player who, Player namedPlayer, String initialName)
-    {
-        super( who );
-        Preconditions.checkNotNull( who, "who" );
-        Preconditions.checkNotNull( namedPlayer, "namedPlayer" );
-        Preconditions.checkNotNull( initialName, "initialName" );
+public class PlayerReceiveNameTagEvent extends PlayerEvent {
 
-        this.namedPlayer = namedPlayer;
-        this.tag = initialName;
-    }
+	private static final HandlerList handlers = new HandlerList();
+	/* ======================================================================== */
+	@Getter
+	private final Player namedPlayer;
+	@Getter
+	private String tag;
+	@Getter
+	private boolean modified;
 
-    public boolean setTag(String tag)
-    {
-        Preconditions.checkNotNull( tag, "tag" );
+	public PlayerReceiveNameTagEvent(Player who, Player namedPlayer, String initialName) {
+		super(who);
+		Preconditions.checkNotNull(who, "who");
+		Preconditions.checkNotNull(namedPlayer, "namedPlayer");
+		Preconditions.checkNotNull(initialName, "initialName");
 
-        this.tag = tag;
-        this.modified = true;
+		this.namedPlayer = namedPlayer;
+		this.tag = initialName;
+	}
 
-        return tag.length() < 16;
-    }
+	public boolean setTag(String tag) {
+		Preconditions.checkNotNull(tag, "tag");
 
-    @Override
-    public HandlerList getHandlers()
-    {
-        return handlers;
-    }
+		this.tag = tag;
+		this.modified = true;
 
-    public static HandlerList getHandlerList()
-    {
-        return handlers;
-    }
+		return tag.length() < 16;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+
+	public String getTag() {
+		return tag;
+	}
 }
